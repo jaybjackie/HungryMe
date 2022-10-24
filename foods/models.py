a51eee5320e68fdf9ea08b3308e0fd9466137233
jaybjackie <kodchakan.forwork@gmail.com>
2022-10-24 15:41:51 +0700

    Allow null value for some data

--- foods/models.py
+++ foods/models.py
@@ -5,12 +5,12 @@ class Menu(models.Model):
     menu_name = models.CharField(max_length=50)
     creator_name = models.CharField(max_length=50, default="Official HungryMe")
     number_of_ingredients = models.IntegerField(default=1)
-    total_cooking_time = models.CharField(max_length=25, default='15 minutes')
-    kcal = models.FloatField(default=0.0)
-    picture_url = models.URLField(max_length=200, default='')
-    rating = models.FloatField(default=0.0)
-    difficulty = models.CharField(max_length=30, default="None")
-    description = models.CharField(max_length=250, default='No description')
+    total_cooking_time = models.CharField(max_length=25, default='15 minutes', null=True)
+    kcal = models.FloatField(default=0.0, null=True)
+    picture_url = models.URLField(max_length=200, default='', null=True)
+    rating = models.FloatField(default=0.0, null=True)
+    difficulty = models.CharField(max_length=30, default="None", null=True)
+    description = models.CharField(max_length=250, default='No description', null=True)
 
     def __str__(self) -> str:
         return f"{self.menu_name} by {self.creator_name}"

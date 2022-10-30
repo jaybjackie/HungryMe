6e80ea958ec4e72a51af4e279b9bc3fc0e7fa1c4
jaybjackie <kodchakan.forwork@gmail.com>
2022-10-30 19:50:48 +0700

    Caching API.

--- foods/urls.py
+++ foods/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:menu_id>/', views.detail, name='detail'),
-    path('search_bar', views.search_bar, name='search-bar')
 ]

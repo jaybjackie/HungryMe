195982f3753c74a0803e15468de9e81c9897827f
jaybjackie <kodchakan.forwork@gmail.com>
2022-11-12 22:48:16 +0700

    Fix #67 Mapping / by precise way

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from foods import views
 
+from django.views.generic import RedirectView
 urlpatterns = [
     path('', include('foods.urls')),
-    path('foods/', include('foods.urls')),
+    path('foods/', RedirectView.as_view(url="/")),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),

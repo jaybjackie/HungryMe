8ca89550ecc3ad15028f71b90279f734ca2e211f
jaybjackie <kodchakan.forwork@gmail.com>
2022-11-22 19:01:37 +0700

    Decorate top-rated dishes, dump new data

--- foods/views.py
+++ foods/views.py
@@ -287,7 +287,7 @@ def cook_create(request):
 
 def top_rate_foods(request):
     menu_list = Menu.objects.all()
-    rated_menu = [Menu.objects.get(pk=menu.id) for menu in menu_list if Menu.objects.get(pk=menu.id).avg_menurating != None]
+    rated_menu = [Menu.objects.get(pk=menu.id) for menu in menu_list if Menu.objects.get(pk=menu.id).avg_menurating != None][:10]
     sort_rated = sorted(rated_menu, key=lambda menu: menu.avg_menurating, reverse=True)
     
     return render(request, 'foods/top_foods.html', {"rated_chart": sort_rated})

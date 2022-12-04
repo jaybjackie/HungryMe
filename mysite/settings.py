32ce9ba66a3f1452a95ceafb291d3614e7c7063a
jaybjackie <kodchakan.forwork@gmail.com>
2022-12-04 10:42:07 +0700

    need to stuck to basic ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config("SECRET_KEY", default="missing-secret-key")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = ["jaybjackie.pythonanywhere.com"]
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', 'jaybjackie.pythonanywhere.com']
 
 
 # Application definition

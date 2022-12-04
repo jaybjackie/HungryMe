fb21f27617ce42c9f6a36f0badeee9d7e2737934
jaybjackie <kodchakan.forwork@gmail.com>
2022-12-04 10:12:37 +0700

    Externalize config plus change it in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", cast=str, default="missing-secret-key")
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config("DEBUG", cast=bool, default=False)
-DEBUG = True
-# add localhost using config
-# ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost")
-ALLOWED_HOSTS = ['.localhost' , '127.0.0.1', '[::1]']
+SECRET_KEY = config("SECRET_KEY", default="missing-secret-key")
+
+DEBUG = config("DEBUG", cast=bool, default=False)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost")
+
 
 # Application definition
 
